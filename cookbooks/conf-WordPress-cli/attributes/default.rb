076fdcc7ced5159ab1c6aacba6f205769eae130a
Hirokazu MORIKAWA <github@nxhack.com>
2012-09-06 07:20:21 +0900

    modify plugins

--- cookbooks/conf-WordPress-cli/attributes/default.rb
+++ cookbooks/conf-WordPress-cli/attributes/default.rb
@@ -3,7 +3,7 @@ default['wp']['title'] = 'TEST site'
 default['wp']['admin_email'] = 'admin@example.com'
 default['wp']['admin_password'] = 'WPAdmin0Pass'
 #
-default['wp']['plugins'] = [ 'apc', '001-prime-strategy-translate-accelerator', 'backwpup', 'easy-fancybox', 'pushpress', 'syntaxhighlighter', 'tinymce-advanced', 'wp-crontrol', 'wp-page-numbers', 'wp-social-bookmarking-light', 'wptouch' ]
+default['wp']['plugins'] = [ 'apc', '001-prime-strategy-translate-accelerator', 'backwpup', 'db-cache-reloaded-fix', 'easy-fancybox', 'pushpress', 'syntaxhighlighter', 'tinymce-advanced', 'wp-crontrol', 'wp-page-numbers', 'wp-social-bookmarking-light', 'wptouch' ]
 #
 # not used
-default['wp']['activate'] = [ 'akismet', 'wp-multibyte-patch', 'apc', '001-prime-strategy-translate-accelerator', 'backwpup', 'easy-fancybox', 'pushpress', 'syntaxhighlighter', 'tinymce-advanced', 'wp-crontrol', 'wp-page-numbers', 'wp-social-bookmarking-light', 'wptouch' ]
+default['wp']['activate'] = [ 'akismet', 'wp-multibyte-patch', '001-prime-strategy-translate-accelerator', 'backwpup', 'db-cache-reloaded-fix', 'easy-fancybox', 'pushpress', 'syntaxhighlighter', 'tinymce-advanced', 'wp-crontrol', 'wp-page-numbers', 'wp-social-bookmarking-light', 'wptouch' ]
